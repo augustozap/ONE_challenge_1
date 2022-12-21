7a0d842ede7142f37665b52659e13c8adeba7828
Augusto Paz <augusto.zap@gmail.com>
2022-12-21 10:46:29 -0600

    Archivos de proyecto con maquetacion inicial auto-response

---
+++ index.js
@@ -0,0 +1,35 @@
+window.onload=inicio;
+function inicio(){
+
+    // document.querySelector(".caja_lateral").insertAdjacentHTML("beforeend",`<img class="boy" src="images/boy.png" alt="boy.png">`);
+    // document.querySelector(".boy").style.position="absolute";
+    // document.querySelector(".boy").style.width="336px";
+    // document.querySelector(".boy").style.height="304px";
+    // document.querySelector(".boy").style.top="283px";
+    // document.querySelector(".boy").style.left="35px";
+
+
+    
+
+    //inserta elemento caja-status
+    // document.querySelector(".caja_lateral").insertAdjacentHTML("beforeend",`
+    // <div class="caja_status">
+    // <p class="status_title">Ningún Mensaje fué encontrado</p>
+    // <p class="status_message">Ingresa el texto que desees encriptar o desencriptar.</p>
+    // </div>`);
+    // document.querySelector(".caja_status").style.display="flex";
+    // document.querySelector(".caja_status").style.flexDirection="column";
+    // document.querySelector(".caja_status").style.alignItems="flexStart";
+    // document.querySelector(".caja_status").style.padding="0px";
+    // document.querySelector(".caja_status").style.gap="16px";
+    // document.querySelector(".caja_status").style.position="absolute";
+    // document.querySelector(".caja_status").style.width="336px";
+    // document.querySelector(".caja_status").style.height="122px"
+    // document.querySelector(".caja_status").style.left="35px";
+    // document.querySelector(".caja_status").style.top="619px";
+
+
+}
+
+
+
